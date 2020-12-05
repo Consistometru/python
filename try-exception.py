f08ed89866f749112894bc5ac004de25a96f2391
Consistometru <63089368+Consistometru@users.noreply.github.com>
2020-12-05 21:54:41 +0200

    Update try-exception.py

--- try-exception.py
+++ try-exception.py
@@ -1,6 +1,6 @@
 print("welcome")
 from operator import pow, truediv, mul, add, sub, floordiv, mod
-
+w=0  #set this to 1 if you want to write to file
 operators = {
   '+': add,
   '-': sub,
@@ -18,11 +18,14 @@ def calculate(s):
         left, operator, right = s.partition(c)
         if operator in operators:
             return operators[operator](calculate(left), calculate(right))
-file = open("history.txt", "w")
+if w:
+    file = open("history.txt", "w")
 input1 = input("continue? yes / no: ")
 while input1 == "yes":
     calc = input("Type calculation:\n")
     print("Answer: " + str(calculate(calc)))
-    file.write(calc + "Answer: " + str(calculate(calc)))
+    if w:
+        file.write(calc + "Answer: " + str(calculate(calc)))
     input1 = input("continue? yes / no: ")
-file.close()
+if w:
+    file.close()
