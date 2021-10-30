5eb500c897da3893dc463e07dd24f3b1fd41b0fe
Consistometru <63089368+Consistometru@users.noreply.github.com>
2021-10-30 16:10:34 +0000

    added update choco

--- update_pip.py
+++ update_pip.py
@@ -4,3 +4,7 @@ pkgs = eval(str(sbp.run("pip3 list -o --format=json", shell=True,
                          stdout=sbp.PIPE).stdout, encoding='utf-8'))
 for pkg in pkgs:
     sbp.run("pip3 install --upgrade " + pkg['name'], shell=True)
+proc = str(sbp.run ( 'choco upgrade all -v ',
+          shell=True, stdout=sbp.PIPE).stdout, )
+print( proc)
+input()
